b395fa462846c5b1c8ab301413cdff83af364217
William Richman <Goat1120@gmail.com>
2020-01-04 14:53:58 -0700

    added files and ajax

---
+++ script.js
@@ -0,0 +1,9 @@
+    const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459"
+    const myURL = ""
+
+$.ajax({
+    url: myURL,
+    method: "GET"
+}).then(function(response){
+
+});
