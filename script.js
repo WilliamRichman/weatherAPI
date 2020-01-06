d7c97507615310ce8db69986b198e21069a67cd9
William Richman <WRichma@mesaaz.gov>
2020-01-06 13:01:42 -0700

    working scripts

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-    const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459"
-    const myURL = ""
+const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459"
+const myURL = ""
+
 
 $.ajax({
-    url: myURL,
+    url: "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459",
     method: "GET"
-}).then(function(response){
+}).then(function (response) {
+    console.log(response)
+    console.log(response.name)
+    console.log(response.main.temp)
+    console.log(response.main.humidity)
+    console.log(response.wind.speed)
+
+
+    const name = response.name
+    console.log("This is the " + name)
+
+    const temp = response.main.temp
+    console.log("This is the " + temp)
+
+    const humid = response.main.humidity
+    console.log("The relative humidity is currently " + humid + "%")
+
+    const winds = response.wind.speed
+    console.log("The wind is blowing like you mom at " + winds + " MPH")
+
+    $("#display").append(name);
+    $("#display").append(temp);
+    $("#display").append(humid);
+    $("#display").append(winds);
+
+
 
 });
+
+
+//function to get the current date and time from system
+
+
+
+
+/* $(document).ready(function () {
+    console.log("ready!");
+}); */
