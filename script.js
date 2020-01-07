6a48ed447f4710bf6b297e8d5cc738da40f825b0
William Richman <WRichma@mesaaz.gov>
2020-01-07 09:20:58 -0700

    added a const zip to link js file and broke the page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const myURL = ""
 
 
 $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459",
+    url: "https://api.openweathermap.org/data/2.5/weather?zip=" + name + ",us&units=imperial&appid=15976d84b292c4206f0104225b002459",
     method: "GET"
 }).then(function (response) {
     console.log(response)
