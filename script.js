0bd930b531bebac7f216209ad7d37e545a91641d
William Richman <Goat1120@gmail.com>
2020-01-08 16:55:13 -0700

    added icons and others

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const weather = "https://api.openweathermap.org/data/2.5/weather?zip=85140,us&units=imperial&appid=15976d84b292c4206f0104225b002459"
-const myURL = ""
 
 
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?zip=" + name + ",us&units=imperial&appid=15976d84b292c4206f0104225b002459",
+
+/* const zip = $('#zip', innerHTML).append("inner.HTML This is not working as of now I will need to fix this later"); */
+
+//setup event listener for enter btn
+
+
+//clear teh from from the first search 
+$("#enter").click(function(){
+  const zip =  $("#zip").val()
+  console.log(zip)
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?zip="+ zip +",us&units=imperial&appid=15976d84b292c4206f0104225b002459",
     method: "GET"
 }).then(function (response) {
     console.log(response)
@@ -12,6 +21,12 @@ $.ajax({
     console.log(response.main.humidity)
     console.log(response.wind.speed)
 
+    const long = response.coord.lon
+    const lat = response.coord.lat
+   
+    uv(long, lat)
+
+    const save = response
 
     const name = response.name
     console.log("This is the " + name)
@@ -29,7 +44,29 @@ $.ajax({
     $(".temp").append("It's currently " + temp + " outside");
     $(".humid").append("Current humidity " + humid + "%");
     $(".winds").append("Current wind speed" + winds);
+
+    localStorage.setItem('storeObj', JSON.stringify(save)); 
 });
+})
+
+function uv(long, lat){
+    const uvLink ="http://api.openweathermap.org/data/2.5/uvi/forecast?appid=15976d84b292c4206f0104225b002459&lat="+ lat + "&lon="+long+"&cnt=0" 
+    $.ajax({
+        url: uvLink,
+        method: "GET"
+
+    }).then(function(response){
+        /* console.log(response[0].value) */
+        const gotUV = response[0].value
+        $(".uv").append("Current UV is " + gotUV);
+        
+    })
+}
+
+
+
+
+
 
 
 //function to get the current date and time from system
